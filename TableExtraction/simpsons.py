50e0a8798470c0f2ad16aef40f5159b20c81cb73
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-03-05 11:53:20 -0600

    Add detailed print statements for Simpsons episode tables extraction

--- TableExtraction/simpsons.py
+++ TableExtraction/simpsons.py
@@ -2,4 +2,20 @@ import pandas as pd
 
 simpsons = pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)')
 
+#print(simpsons[1].head())
+print(len(simpsons)) # Número de tablas en la página
+print(simpsons[1].columns)
+print(simpsons[1].columns.values)
+print(simpsons[1].shape)
+print(simpsons[1].info())
+print(simpsons[1].describe())
+
 print(simpsons[1].head())
+
+print(simpsons[1])
+
+print(simpsons[20])
+
+for i in range(len(simpsons)):
+    print(simpsons[i])
+    
