72233e0a920299df4bcf74356b248dd095de9719
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-03-14 21:44:54 -0600

    Add initial implementation for Algorithmic Trading with stock data retrieval

---
+++ AlgorithmicTrading/main.py
@@ -0,0 +1,15 @@
+# import numpy as np #The Numpy numerical computing library
+# import pandas as pd #The Pandas data science library
+import requests #The requests library for HTTP requests in Python
+# import xlsxwriter #The XlsxWriter libarary for 
+# import math #The Python math module
+
+# stocks = pd.read_csv('sp_500_stocks.csv')
+# print(stocks.head())
+
+from secrets import IEX_CLOUD_API_TOKEN
+
+symbol='AAPL'
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+data = requests.get(api_url).json()
+print(data)
