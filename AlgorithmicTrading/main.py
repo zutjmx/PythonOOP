86a0bd244e91d33df3a9fb27019b63366cb996bc
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-03-15 19:34:17 -0600

    Uncomment code for stock data retrieval in Algorithmic Trading implementation

--- AlgorithmicTrading/main.py
+++ AlgorithmicTrading/main.py
@@ -1,11 +1,11 @@
-# import numpy as np #The Numpy numerical computing library
-# import pandas as pd #The Pandas data science library
+import numpy as np #The Numpy numerical computing library
+import pandas as pd #The Pandas data science library
 import requests #The requests library for HTTP requests in Python
-# import xlsxwriter #The XlsxWriter libarary for 
-# import math #The Python math module
+import xlsxwriter #The XlsxWriter libarary for 
+import math #The Python math module
 
-# stocks = pd.read_csv('sp_500_stocks.csv')
-# print(stocks.head())
+stocks = pd.read_csv('sp_500_stocks.csv')
+print(stocks.head())
 
 from secrets import IEX_CLOUD_API_TOKEN
 
