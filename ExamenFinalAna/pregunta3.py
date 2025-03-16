0c103d94309d2fddb4fa66d68d83762533e06672
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-03-15 20:21:14 -0600

    Add script to display book titles from a predefined list

---
+++ ExamenFinalAna/pregunta3.py
@@ -0,0 +1,6 @@
+libros = [["El Quijote", "Migguel de Cervantes", 1605],
+          ["El principito", "Antoine de Saint-Exupéry", 1943],
+          ["Don Quijote", "Miguel de Cervantes", 1605]]
+for libro in libros:
+    print(libro[0])
+    
